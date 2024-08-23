7e2ab27912e2bf6c019a1a7acb4333e2116ccc2b
Sam Gaudet <32820645+samgaudet@users.noreply.github.com>
2024-08-22 18:41:27 -0600

    Prepare version for release

--- mkdocs_madlibs/__init__.py
+++ mkdocs_madlibs/__init__.py
@@ -1,5 +1,5 @@
 """mkdocs-madlibs Code templating with user inputs for MkDocs superfences."""
 from mkdocs_madlibs._fence import fence
 
-__version__ = "0.0.1-rc1"
+__version__ = "1.0.0"
 __all__ = ["fence"]
